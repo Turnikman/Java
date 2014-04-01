5c4629748e8c88de4051ae27f8c0afa3e8f03d33
Aleksey <turnikman.als@gmail.com>
2014-04-01 21:41:14 +0300

    sixth commit

--- Lab3-4_1/src/lab3/pkg4_1/Lab34_1.java
+++ Lab3-4_1/src/lab3/pkg4_1/Lab34_1.java
@@ -3,8 +3,35 @@ package lab3.pkg4_1;
 public class Lab34_1 {
 
     public static void main(String[] args) {
-       int[] Arr = {1,2,3,4,5,6,7,8,9};
+        int i, min, imin, max, imax;
+        long sum = 0;
+        int[] Arr = new int[]{3, 15, 23, 12, 84, 76, 35, 19, 44, 98, 34, 67};
+        System.out.println("Выводим данные массива на экран по 3 числа в строке: \\t");
+        System.out.println();
+        for (i = 0; i < Arr.length; i++) {
+            System.out.println("Arr(" + i + ")" + Arr[i] + "\t");
+            if ((i + 1) % 3 == 0) {
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println("Умножаем данные массива на 2: \t");
 
+            System.out.println();
+            for (i = 0; i < Arr.length; i++) {
+                Arr[i] *= 2;
+            }
+            for (i = 0; i < Arr.length; i++) {
+                System.out.println("Arr(" + i + ")" + Arr[i] + "\t");
+                if ((i + 1) % 3 == 0) {
+                    System.out.println();
+                }
+                System.out.println();
+                System.out.println("Сумма всех данных массива: \\t");
+                System.out.println();
+
+            }
+
+        }
     }
 
 }
